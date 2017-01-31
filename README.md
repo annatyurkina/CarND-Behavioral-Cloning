ee284bddb014d24359cd2013924c3928724243fc
annatyurkina <atnoon6@gmail.com>
2017-01-31 23:22:38 +0000

    cannot turn after bridge but is trying to 2 epochs

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# CarND-Behavioral-Cloning
+# Behavioral Cloning
+
+The purpose of the model is to be able to pass first track in the Udacity simulator. Simulator uses train mode to record training data, images, steering and other parameters, and autonomous mode for using trained model to produce steering in response to simulator images.
+
+## Image Preprocessing
+
+Udacity provided data is used as a starting point for training the model. We take all center camera images with steering untouched. Then we try to make the data more balanced by introducing more examples with larger steering. For steering > 0.1 we also take left and right camera images. To adjust left and right camera shift, steering is increased by 0.25 and -0.25 respectively. For steering > 0.1 we also apply steering augmentation by random value up to 0.1 for all cameras and append augmented data to our training set. 
+
+Training set is shuffled before validation split and reshuffled in every epoch.
+
+<p align="center">
+  <img src="data/IMG/left_2016_12_01_13_30_48_287.jpg" width="30%"/>
+  <img src="data/IMG/center_2016_12_01_13_30_48_287.jpg" width="30%"/>
+  <img src="data/IMG/right_2016_12_01_13_30_48_287.jpg" width="30%"/>
+</p>
+
+## Model Fitting
+
+Fit_generator function from Keras library is used to fit the model in memory efficient way: it loads images batch by batch in parallel with the model training.
+
+The data is normalised in the model using Lambda function before the first convolution.
+
+## Network Architecture
+
+Network architecture is based on the network from NVIDIA article http://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf 
+
+It starts with a set of convolutional layers to introduce non-linearity to the model. First, there are three 5 by 5 convolutions with stride 2; first layer has valid padding and latter two layers have same padding. The depth of convolutions are 24, 36 and 48 respectively.
+
+Then it is followed by two convolutional layers 3 by 3, depth 64, same padding.
+
+Every convolution is activated with RELU.
+
+Then network continues with 3 fully connected layers with 100, 50 and 10 neurons respectively. They are followed by 0.5 dropout layer to avoid overfitting.
+
+Output layer with single neuron produces single output for predicted steering.
+
+The model is using Adam optimiser (improved version of Gradient Descent) to minimise minimum square error loss function.
+
+
+
+
--- model.py
+++ model.py
@@ -6,6 +6,7 @@ import cv2
 import datetime
 import matplotlib.image as mpimg
 import numpy as np
+import matplotlib.pyplot as plt
 from random import randint
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
@@ -15,50 +16,129 @@ from keras.layers.core import Lambda
 
 BATCH_SIZE = 128
 NUMBER_EPOCHS = 2
+VERBOSE = False
 
 def load_image_paths_and_steering():
 	image_paths, steering = [], []
+	image_paths_val, steering_val = [], []
 	first_row = True
 
 	with open('data/driving_log.csv', 'r') as csvfile:
 		logreader = csv.reader(csvfile, delimiter=',')
+
 		for row in logreader:
+			# skip header row
 			if(first_row):
 				first_row = False
 				continue
+
 			steering_lable = float(row[3].strip())
 			augmented_steering_lable = 0.0
-			image_paths.append(row[0].strip())
-			steering.append(steering_lable) 
-			if(abs(steering_lable) > .1):
+
+			if(steering_lable == 0.0 and randint(0, 3) > 0):
+				continue
+
+			if(randint(0, 2) == 0):
+				validation = True
+			else:
+				validation = False
+			
+			if(validation):
+				image_paths_val.append(row[0].strip())
+				steering_val.append(steering_lable)
+				#if(steering_lable > .0):
+				#image_paths_val.append(row[1].strip())
+				#steering_val.append(steering_lable + .25) 
+				#if(steering_lable < .0):
+				#image_paths_val.append(row[2].strip())
+				#steering_val.append(steering_lable - .25) 
+			else:
+				image_paths.append(row[0].strip())
+				steering.append(steering_lable)
+				#if(steering_lable > .0):
 				image_paths.append(row[1].strip())
 				steering.append(steering_lable + .25) 
+				#if(steering_lable < .0):
 				image_paths.append(row[2].strip())
 				steering.append(steering_lable - .25) 
-			if(steering_lable > .1):
-				augmented_steering_lable = steering_lable + randint(1,10)/100
-			if(steering_lable < -.1):
-				augmented_steering_lable = steering_lable - randint(1,10)/100
-			if(abs(augmented_steering_lable) > .1):
-				image_paths.append(row[0].strip())
-				steering.append(augmented_steering_lable) 
-				image_paths.append(row[1].strip())
-				steering.append(augmented_steering_lable + .25) 
-				image_paths.append(row[2].strip())
-				steering.append(augmented_steering_lable - .25) 
-			# if(steering_lable > .1):
-			# 	augmented_steering_lable = steering_lable + randint(1,10)/100
-			# if(steering_lable < -.1):
-			# 	augmented_steering_lable = steering_lable - randint(1,10)/100
-			# if(abs(augmented_steering_lable) > .1):
-			# 	image_paths.append(row[0].strip())
-			# 	steering.append(augmented_steering_lable) 
-			# 	image_paths.append(row[1].strip())
-			# 	steering.append(augmented_steering_lable + .25) 
-			# 	image_paths.append(row[2].strip())
-			# 	steering.append(augmented_steering_lable - .25) 
 
-	return (image_paths, steering)
+				# if(steering_lable > .0):
+				# 	image_paths.append(row[0].strip())
+				# 	steering.append(steering_lable  + randint(1,10)/100) 
+				# 	image_paths.append(row[1].strip())
+				# 	steering.append(steering_lable + randint(15,35)/100) 
+				# if(steering_lable < .0):
+				# 	image_paths.append(row[0].strip())
+				# 	steering.append(steering_lable  - randint(1,10)/100) 
+				# 	image_paths.append(row[2].strip())
+				# 	steering.append(steering_lable - randint(15,35)/100) 
+
+				if(steering_lable > .0):
+					image_paths.append(row[0].strip())
+					steering.append(steering_lable + .1) 
+					image_paths.append(row[1].strip())
+					steering.append(steering_lable + .35) 
+					# image_paths.append(row[0].strip())
+					# steering.append(steering_lable + .1) 
+					# image_paths.append(row[1].strip())
+					# steering.append(steering_lable + .30) 
+				if(steering_lable < .0):
+					image_paths.append(row[0].strip())
+					steering.append(steering_lable  - .1) 
+					image_paths.append(row[2].strip())
+					steering.append(steering_lable - .35) 
+				# 	image_paths.append(row[0].strip())
+				# 	steering.append(steering_lable - .1) 
+				# 	image_paths.append(row[1].strip())
+				# 	steering.append(steering_lable - .30) 
+
+				# if(steering_lable > .1):
+				# 	image_paths.append(row[0].strip())
+				# 	steering.append(steering_lable  + randint(1,10)/100) 
+				# 	image_paths.append(row[1].strip())
+				# 	steering.append(steering_lable + randint(10,35)/100) 
+				# if(steering_lable < .1):
+				# 	image_paths.append(row[0].strip())
+				# 	steering.append(steering_lable  - randint(1,10)/100) 
+				# 	image_paths.append(row[2].strip())
+				# 	steering.append(steering_lable - randint(15,35)/100) 
+
+	#X_train, X_validation, y_train, y_validation = train_test_split(X, y, test_size=0.3)
+	X_train, X_validation, y_train, y_validation = np.array(image_paths), np.array(image_paths_val), np.array(steering), np.array(steering_val)	
+	print(len(y_validation))
+	print(len(y_train))	
+
+			# add center camera data, non-augmented
+
+			# add left and right camera data for larger steering angles
+			# steering label is adjusted to cater for left and right camera shifts 
+			#if(abs(steering_lable) > .1):
+
+			# randomly augment steering angles for larger values and append to provided data 
+				# if(steering_lable > .1):
+				# 	augmented_steering_lable = steering_lable + randint(1,10)/100
+				# if(steering_lable < -.1):
+				# 	augmented_steering_lable = steering_lable - randint(1,10)/100
+			#if(abs(augmented_steering_lable) > .1):
+				# image_paths.append(row[0].strip())
+				# steering.append(augmented_steering_lable) 
+				# image_paths.append(row[1].strip())
+				# steering.append(augmented_steering_lable + .25) 
+				# image_paths.append(row[2].strip())
+				# steering.append(augmented_steering_lable - .25) 
+
+
+	X_train, y_train = shuffle(X_train, y_train)
+	X_validation, y_validation = shuffle(X_validation, y_validation)
+	a = np.array(y_train)
+	plt.hist(a, bins=60)
+	plt.show()
+	X_validation, y_validation = shuffle(X_validation, y_validation)
+	a = np.array(y_validation)
+	plt.hist(a, bins=60)
+	plt.show()
+
+	return (X_train, X_validation, y_train, y_validation)
 
 def train_validation_split(X, y):
 	X, y = shuffle(X, y)
@@ -66,27 +146,30 @@ def train_validation_split(X, y):
 
 	return (X_train, X_validation, y_train, y_validation)
 
-def batch_generator(X, y):
-	first_time = True
+def batch_generator(X, y, isvalidation = False):
 	example_count = len(X)
 	while True:
+		X, y = shuffle(X, y)
+
 		for offset in range(0, example_count, BATCH_SIZE):
-			batch_x, batch_y = X[offset:min(offset+BATCH_SIZE, example_count)], y[offset:min(offset+BATCH_SIZE, example_count)]        
+			batch_x, batch_y = X[offset:min(offset+BATCH_SIZE, example_count)], y[offset:min(offset+BATCH_SIZE, example_count)]   
+
 			images, labels = [], []
 			for i in range(0, len(batch_x)):
 				image = mpimg.imread('data/' + batch_x[i])
-				# if(randint(0,1) > 0):
-				# 	image = cv2.flip(image, 1)
-				# 	batch_y[i] = -batch_y[i]
+				#random 0.2 horisontal flip
+				#if(isvalidation == False and abs(batch_y[i]) > 0.2 and randint(0,1) == 0):
+				#	image = cv2.flip(image, 1)
+				#	batch_y[i] = -batch_y[i]
 				images.append(image)
 				labels.append(batch_y[i])
 
 			X_batch = np.array(images)
-			if(first_time):
-				first_time = False
-				print(X_batch.shape)
-			#X_batch = np.divide(np.add(X_batch, -128),128)
 			y_batch = np.array(labels)
+
+			if(VERBOSE):
+				print(X_batch.shape)
+
 			yield(X_batch, y_batch)
 
 
@@ -121,12 +204,15 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Behavior Cloning')
 	parser.add_argument('--batch', type=int, default=128, help='Batch Size')
 	parser.add_argument('--epoch', type=int, default=2, help='Number of Epochs')
+	parser.add_argument('--verbose', type=bool, default=False, help='Verbose')
 	args = parser.parse_args()
 	BATCH_SIZE = args.batch
 	NUMBER_EPOCHS = args.epoch
+	VERBOSE = args.verbose
 
-	image_paths, steering = load_image_paths_and_steering()
-	X_train, X_validation, y_train, y_validation = train_validation_split(np.array(image_paths), np.array(steering))
+	X_train, X_validation, y_train, y_validation = load_image_paths_and_steering()
+	#X_train, X_validation, y_train, y_validation = train_validation_split(np.array(image_paths), np.array(steering))
+
 	print(len(X_train))	
 	print(datetime.datetime.utcnow())
 
@@ -138,6 +224,7 @@ if __name__ == "__main__":
 		validation_data=batch_generator(X_validation, y_validation),
 		nb_val_samples=len(X_validation)
 		)
+
 	print(datetime.datetime.utcnow())
 	print("Saving model weights and configuration file.")
 
